19fb8f068433089cef5a7547be060f632e327b00
az <aziemski@github.com>
2024-11-16 20:58:35 +0100

    Save isListView in local store

--- src/store.js
+++ src/store.js
@@ -1,8 +1,33 @@
-import {reactive} from 'vue';
+import {reactive, watch} from "vue";
+
+const savedState = JSON.parse(localStorage.getItem("store")) || {};
 
 export const store = reactive({
-    isListView: true,
+    isListView:
+        savedState.isListView
+        !== undefined
+            ? savedState.isListView : false,
+
+    isAuthenticated:
+        savedState.isAuthenticated !== undefined
+            ? savedState.isAuthenticated : false,
+
     toggleView() {
         this.isListView = !this.isListView;
-    }
+    },
+
+    setIsAuthenticated(status) {
+        this.isAuthenticated = status;
+    },
 });
+
+watch(
+    () => ({
+        isListView: store.isListView,
+        isAuthenticated: store.isAuthenticated
+    }),
+    (newValues) => {
+        localStorage.setItem("store", JSON.stringify(newValues));
+    },
+    {deep: true}
+);
